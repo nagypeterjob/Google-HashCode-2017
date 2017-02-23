197b7d62ae620a5b8bc1b44f41bb6c48557919dd
Norbert Bajkó <norbert.bajko94@gmail.com>
2017-02-23 22:38:18 +0100

    + after time :(

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import read
 from collections import OrderedDict
+import difflib
 
 
 def priority_videos(videos):
     for x in videos:
         x['priority'] = x['request_number'] * x['size']
+        x['ideal'] = {}
         # print(x)
         # print(x['priority'])
     prio_videos = sorted(videos, key=lambda video: video[
@@ -21,12 +23,14 @@ def avg_cache_delay(endpoints, size):
             if y['id'] in avg_delays:
                 avg_delays[y['id']]['num'] += 1
                 avg_delays[y['id']]['total_delay'] += y['latency']
+                avg_delays[y['id']]['users'].append(x['id'])
             else:
                 avg_delays[y['id']] = {}
                 avg_delays[y['id']]['id'] = y['id']
                 avg_delays[y['id']]['num'] = 1
                 avg_delays[y['id']]['total_delay'] = y['latency']
                 avg_delays[y['id']]['rem_size'] = size
+                avg_delays[y['id']]['users'] = [x['id']]
     for x in avg_delays:
         avg_delays[x]['avg_delay'] = avg_delays[x][
             'total_delay'] / avg_delays[x]['num']
@@ -40,8 +44,25 @@ def avg_cache_delay(endpoints, size):
 def optimize(videos, caches2):
     tmp = {}
     for x in videos:
-        for y in caches2:
-            # print(y)
+        best = 0
+        best_1 = 0
+        best_val = 10000000
+        for z in x['ideal']:
+            if x['ideal'][z] < best_val:
+                best_1 = best
+                best = z
+        # print(x['ideal'][best])
+        y = caches2[best]
+        # print(y)
+        if y['rem_size'] >= x['size']:
+            # print(y['id'])
+            if y['id'] in tmp:
+                tmp[y['id']].append(x['id'])
+            else:
+                tmp[y['id']] = [x['id']]
+            y['rem_size'] -= x['size']
+        else:
+            y = caches2[best_1]
             if y['rem_size'] >= x['size']:
                 # print(y['id'])
                 if y['id'] in tmp:
@@ -49,14 +70,37 @@ def optimize(videos, caches2):
                 else:
                     tmp[y['id']] = [x['id']]
                 y['rem_size'] -= x['size']
-                break
     return tmp
+
+
+def optimize2(videos, caches):
+    for x in videos:
+        # print(x['id'])
+        for y in caches:
+            tmp = difflib.SequenceMatcher(
+                None, x['requests'], y['users']).ratio()
+            if tmp == 0:
+                # print(x['ideal'])
+                x['ideal'][y['id']] = 10000000
+            else:
+                x['ideal'][y['id']] = y['avg_delay'] / \
+                    min(1, (difflib.SequenceMatcher(
+                        None, x['requests'], y['users']).ratio() * 2))
+
+        # print(x)
+        # print()
+    return videos
+
 
 [input_array, videos, endpoints] = read.readFile('videos_worth_spreading.in')
 
 videos = priority_videos(videos)
 caches2 = avg_cache_delay(endpoints, input_array[4])
 
+videos = optimize2(videos, caches2)
+
+# for x in videos:
+#     print(x['ideal'])
 result = optimize(videos, caches2)
 # for x in result:
 #     print(x, result[x])
